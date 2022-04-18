ff28e393a164f4b671c1197cb104910f729530ba
ayrtonbolanos <ayrtonbolanos.anbo@gmail.com>
2022-04-18 09:49:56 -0500

    Página registrar y creación de los componentes por página.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { ProductoComponent } from './producto/producto.component';
+import { IniciasesComponent } from './iniciases/iniciases.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { NotaComponent } from './nota/nota.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    RegistrarseComponent,
+    ProductoComponent,
+    IniciasesComponent,
+    CarritoComponent,
+    NotaComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/iniciases/iniciases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iniciases',
+  templateUrl: './iniciases.component.html',
+  styleUrls: ['./iniciases.component.css']
+})
+export class IniciasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
