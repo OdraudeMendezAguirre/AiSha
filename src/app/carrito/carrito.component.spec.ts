fab8b07916c708fec435403bdeaf2b30030736bc
OdraudeMendezAguirre <mendez.odraude.1dm@gmail.com>
2022-05-23 22:21:06 -0500

    Closes #17

--- src/app/carrito/carrito.component.spec.ts
+++ src/app/carrito/carrito.component.spec.ts
@@ -19,7 +19,5 @@ describe('CarritoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
--- src/app/index/index.component.spec.ts
+++ src/app/index/index.component.spec.ts
@@ -19,7 +19,4 @@ describe('IndexComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
